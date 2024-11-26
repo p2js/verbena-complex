887f68192155ba94b6b6610289354acaff47a1ba
Alfio <tomarchioalfio5@gmail.com>
2024-11-26 00:23:03 +0000

    add comments, complex functions

--- src/index.ts
+++ src/index.ts
@@ -6,35 +6,54 @@ import { parse } from 'verbena/parser';
 import { compileFn } from 'verbena/compileFn';
 import { TokenType } from 'verbena/token';
 
-function enforceRealFn(f: (...args: number[]) => number): (...args: Complex[]) => Complex {
-    return (...args) => {
-        let realArgs = args.map(z => {
-            z = Complex(z);
-            if (z.im != 0) {
-                throw Error("complex " + f.name + "is unsupported");
-            }
-            return z.re;
-        });
-
-        return Complex(f(...realArgs));
-    }
-}
-
+/**
+ * Library for complex functions.
+ * 
+ * Includes complex operations and functions, as well as constants for i, pi and e.
+ */
 export const complexLib: Library<Complex> = {
     operations: {
-        add: (l, r) => Complex(l).add(Complex(r)),
-        sub: (l, r) => Complex(l).sub(Complex(r)),
-        mul: (l, r) => Complex(l).mul(Complex(r)),
-        div: (l, r) => Complex(l).div(Complex(r)),
-        pow: (l, r) => Complex(l).pow(Complex(r)),
-        neg: (x) => Complex(x).mul(-1),
-        abs: (x) => Complex(Complex(x).abs()),
+        add: forceComplex((l, r) => l.add(r)),
+        sub: forceComplex((l, r) => l.sub(r)),
+        mul: forceComplex((l, r) => l.mul(r)),
+        div: forceComplex((l, r) => l.div(r)),
+        pow: forceComplex((l, r) => l.pow(r)),
+        neg: forceComplex((z) => z.mul(-1)),
 
-        mod: enforceRealFn((x, y) => x % y),
-        fac: enforceRealFn(std.functions.fac)
+        mod: forceReal((x, y) => x % y),
+        fac: forceReal(std.functions.fac)
     },
     functions: {
+        re: forceComplex((z) => Complex(z.re)),
+        im: forceComplex((z) => Complex(0, z.im)),
 
+        abs: forceComplex((z) => Complex(z.abs())),
+        acos: forceComplex((z) => z.acos()),
+        acosh: forceComplex((z) => z.acosh()),
+        asin: forceComplex((z) => z.asin()),
+        asinh: forceComplex((z) => z.asinh()),
+        atan: forceComplex((z) => z.atan()),
+        atanh: forceComplex((z) => z.atanh()),
+        cbrt: forceComplex((z) => z.pow(1 / 3)),
+        ceil: forceComplex((z) => z.ceil(0)),
+        cos: forceComplex((z) => z.cos()),
+        cosh: forceComplex((z) => z.cosh()),
+        exp: forceComplex((z) => z.exp()),
+        floor: forceComplex((z) => z.floor(0)),
+        hypot: forceComplex((...zs) => zs.reduce((acc, z) => acc.add(z.pow(2)), Complex(0)).sqrt()),
+        log_: forceComplex((z, b = Complex(10)) => z.log().div(b.log())),
+        ln: forceComplex((z) => z.log()),
+        // max: forceComplex((z) => z.max()),    -> Should these be implemented by magnitude?
+        // min: forceComplex((z) => z.min()),
+        pow: forceComplex((z, p) => z.pow(p)),
+        random: () => Complex(Math.random()),
+        round: forceComplex((z) => z.round(0)),
+        sign: forceComplex((z) => z.sign()),
+        sin: forceComplex((z) => z.sin()),
+        sinh: forceComplex((z) => z.sinh()),
+        sqrt: forceComplex((z) => z.sqrt()),
+        tan: forceComplex((z) => z.tan()),
+        tanh: forceComplex((z) => z.tanh())
     },
     constants: {
         i: Complex.I,
@@ -45,7 +64,7 @@ export const complexLib: Library<Complex> = {
 
 export function ComplexFunction(source: string): vbFunction<Complex> {
     let tokens = scan(source, complexLib as any).map(token => {
-        // Little hack to allow multiplications of identifiers with i, like ix
+        // Little hack to allow implicit of identifiers with i, like ix
         if (token.type == TokenType.IDENTIFIER && token.lexeme == 'i') {
             token.type = TokenType.CONSTANT;
         }
@@ -53,10 +72,16 @@ export function ComplexFunction(source: string): vbFunction<Complex> {
     });
     let decl = parse(tokens);
 
+    // Remove function clauses
+    decl.clauses = [];
+
+    // Compile the function as a real function using the complex library
     let compiledFn = compileFn(decl, complexLib as any);
 
-    let complexFn = ((...args: Complex[]) => Complex(compiledFn(...args as any[]))) as vbFunction<Complex>;
+    // Force conversion of function arguments and result to a complex number
+    let complexFn = forceComplex((...args) => Complex(compiledFn(...args as any[]))) as vbFunction<Complex>;
 
+    // Copy over the real function's properties to the complex one
     Object.defineProperties(complexFn, {
         name: {
             value: compiledFn.name,
@@ -81,4 +106,34 @@ export function ComplexFunction(source: string): vbFunction<Complex> {
     });
 
     return complexFn;
+}
+
+/**
+ * Utility function to wrap a complex function, converting all its arguments to a Complex value.
+ * 
+ * @param f Complex function
+ * @returns `f` with all its parameters converted to Complex
+ */
+function forceComplex(f: (...args: Complex[]) => Complex): (...args: Complex[]) => Complex {
+    return (...args: Complex[]) => f(...args.map(z => Complex(z)));
+}
+
+/**
+ * Utility function to wrap a real function, forcing all its arguments to be real numbers.
+ * 
+ * @param f Real function
+ * @returns `f`, but will throw an error on any complex arguments
+ */
+function forceReal(f: (...args: number[]) => number): (...args: Complex[]) => Complex {
+    return (...args) => {
+        let realArgs = args.map(z => {
+            z = Complex(z);
+            if (z.im != 0) {
+                throw Error("complex " + f.name + "is unsupported");
+            }
+            return z.re;
+        });
+
+        return Complex(f(...realArgs));
+    }
 }
